7034059709b94ee9a8daf8e0087b6d9678aac0c3
EfsaneHesenova <160878199+EfsaneHesenova@users.noreply.github.com>
2024-10-02 10:50:07 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿float gpa = 93.8f;
+
+switch (gpa)
+{
+    case float n when (n <= 100f && n >= 91f):
+        Console.WriteLine("A");
+        break;
+    case float n when (n >= 81f && n < 91f):
+        Console.WriteLine("B");
+        break;
+    case float n when (n >= 71f && n < 81f):
+        Console.WriteLine("C");
+        break;
+    case float n when (n >= 61f && n < 71f):
+        Console.WriteLine("D");
+        break;
+    case float n when (n >= 51f && n < 61f):
+        Console.WriteLine("E");
+        break;
+    default:
+        Console.WriteLine("F");
+        break;
+}
